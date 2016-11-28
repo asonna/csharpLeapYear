31028636faecc6549c6d91bb76619631ff9cab54
Annie Sonna and Anthony Bottemiller <annie+anthony@>
2016-11-28 09:22:25 -0800

    initial commit

---
+++ Objects/leapYear.cs
@@ -0,0 +1,11 @@
+namespace LeapYear
+{
+  public class LeapYear
+  {
+    public bool IsLeapYear(int year)
+    {
+      //Code will go here
+      return false;
+    }
+  }
+}
---
+++ Tests/LeapYearTest.cs
@@ -0,0 +1,12 @@
+using Xunit;
+namespace LeapYear
+{
+  public class LeapYearTest
+  {
+    [Fact]
+    public void IsLeapYear_ForNumberDivisibleByFour_true()
+    {
+      // eventually your testing code will go here
+    }
+  }
+}
